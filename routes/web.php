9b057949942483b50e8076e9e43ad434d03869e0
Argenis Osorio <argenisosorio580@hotmail.com>
2022-03-05 08:38:42 -0400

    Generada estructura básica de autenticación con laravel ui, plantillas, rutas, controladores, configuraciones, entre otras

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 
 
 Route::resource('projects', ProjectController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
